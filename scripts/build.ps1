dca30c14eefcff9a33a564217a55db629bff4cf1
Matej Kubinec <matej.kubinec@outlook.com>
2021-05-17 18:15:36 +0200

    Rewrite build script & add timestamp

--- scripts/build.ps1
+++ scripts/build.ps1
@@ -1,15 +1,21 @@
-$result = @{}
+# Write timestamp file
+Get-Date -Format 'o' | Set-Content '../dist/timestamp.txt';
+
+# Write songbook file
+$songbook = @();
 $artists = Get-ChildItem -Path '../songs';
 
 foreach ($artist in $artists) {
-    $result.Add($artist.Name, @{})
-
     $songs = Get-ChildItem -Path $artist
 
     foreach ($song in $songs) {
-        $content = Get-Content $song -Raw;
-        $result[$artist.Name].Add($song.Name, $content)
+        $content = Get-Content $song -Raw | Out-String
+        $songbook += @{
+            artist  = $artist.BaseName
+            title   = $song.BaseName
+            content = $content
+        }
     }
 }
 
-$result | ConvertTo-Json | Set-Content '../dist/songbook.json';
+$songbook | ConvertTo-Json | Set-Content -Path '../dist/songbook.json' -Force;
